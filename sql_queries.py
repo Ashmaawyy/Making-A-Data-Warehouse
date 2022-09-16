8d3c09cdf8b5833e61ec8464b2ff0897f438cad4
Ashmaawyy <al_ashmawy@outlook.com>
2022-09-16 17:47:34 +0200

    Added insert statements : )

--- sql_queries.py
+++ sql_queries.py
@@ -82,24 +82,62 @@ staging_songs_copy = ("""
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
+user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) \
+    VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id)
+    DO UPDATE SET level = EXCLUDED.level;
 """)
 
-user_table_insert = ("""
+song_table_insert = (""" INSERT INTO songs (artist_id, song_id, title, duration, year) \
+    VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
 """)
 
-song_table_insert = ("""
+artist_table_insert = (""" INSERT INTO artists (artist_id, latitude, longitude, location, name) \
+    VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;
 """)
 
-artist_table_insert = ("""
+time_table_insert = (""" INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
+    VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time)
+    DO UPDATE SET start_time = EXCLUDED.start_time;
 """)
 
-time_table_insert = ("""
+songplay_table_insert = (""" INSERT INTO songplays (start_time,
+                                                    user_id,
+                                                    level,
+                                                    session_id,
+                                                    location,
+                                                    user_agent,
+                                                    song_id,
+                                                    artist_id) \
+                                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s) \
+                                                    ON CONFLICT DO NOTHING;
 """)
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+create_table_queries = [
+    staging_events_table_create,
+    staging_songs_table_create, user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create,
+    songplay_table_create]
+
+drop_table_queries = [
+    staging_events_table_drop,
+    staging_songs_table_drop,
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop]
+
+copy_table_queries = [
+    staging_events_copy,
+    staging_songs_copy]
+
+insert_table_queries = [
+    user_table_insert,
+    song_table_insert,
+    artist_table_insert,
+    time_table_insert,
+    songplay_table_insert]
