5dae858216517beb0c0fa7f2e8fbe05bdf4d9a01
Ashmaawyy <al_ashmawy@outlook.com>
2022-09-18 05:46:44 +0200

    Added docstrings and try except blocks : )

--- create_tables.py
+++ create_tables.py
@@ -4,22 +4,54 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+    """
+    Drops tables if they exist
+    """
+    try:
+        for query in drop_table_queries:
+            cur.execute(query)
+            conn.commit()
+
+        print('Dropped tables successfully :)')
+
+    except psycopg2.Error as e:
+        print(e)
 
 
 def create_tables(cur, conn):
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+    """
+    Creates tables if they don't exist
+    """
+    try:
+        for query in create_table_queries:
+            cur.execute(query)
+            conn.commit()
+        
+        print('Created tables scuccessfuly :)')
+    
+    except psycopg2.Error as e:
+        print(e)
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    """
+    Drops existing tables and then creates new tables
+    """
+    try:
+        config = configparser.ConfigParser()
+        config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+        conn = psycopg2.connect("""host={} 
+                                   dbname={} 
+                                   user={} 
+                                   password={} 
+                                   port={}""".format(*config['CLUSTER'].values()))
+
+        print('Connected to Database successfully :)')
+
+    except psycopg2.Error as e:
+        print(e)
+    
     cur = conn.cursor()
 
     drop_tables(cur, conn)
--- etl.py
+++ etl.py
@@ -4,12 +4,18 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data from S3 bucket to the two staging tables (logs_staging_tables, songs_statging_tables)
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Loads data from the two staging tables to the database tables
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
@@ -19,7 +25,18 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    try:
+        conn = psycopg2.connect("""host={} 
+                                   dbname={} 
+                                   user={} 
+                                   password={} 
+                                   port={}""".format(*config['CLUSTER'].values()))
+
+        print('Connected to Database successfully :)')
+
+    except psycopg2.Error as e:
+        print(e)
+    
     cur = conn.cursor()
     
     load_staging_tables(cur, conn)
