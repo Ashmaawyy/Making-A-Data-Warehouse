83502c7603edba857aee60901de722b54b141115
Ashmaawyy <al_ashmawy@outlook.com>
2022-09-18 09:41:20 +0200

    Corrected indent : )

--- sql_queries.py
+++ sql_queries.py
@@ -83,9 +83,9 @@ staging_events_copy = (""" COPY staging_events
 """).format(config['S3']['LOG_DATA'], *config['IAM_ROLE'].values())
 
 staging_songs_copy = (""" COPY staging_songs
-                           FROM {}
-                           IAM_ROLE {}
-                           REGION 'us-east-1';
+                          FROM {}
+                          IAM_ROLE {}
+                          REGION 'us-east-1';
 """).format(config['S3']['SONG_DATA'], *config['IAM_ROLE'].values())
 
 # FINAL TABLES
