c1acb2308aafefa1ad9ed713e0da365ca8a32f7d
Ashmaawyy <al_ashmawy@outlook.com>
2022-09-18 09:32:52 +0200

    Added CASCADE to artists and songs tables to drop their dependancies : )

--- sql_queries.py
+++ sql_queries.py
@@ -11,8 +11,8 @@ staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
 songplay_table_drop = "DROP TABLE IF EXISTS users"
 user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
+song_table_drop = "DROP TABLE IF EXISTS songs CASCADE"
+artist_table_drop = "DROP TABLE IF EXISTS artists CASCADE"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
